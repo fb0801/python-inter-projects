92bd479558214fe8a26b72d7d9a11cfc441ec0e1
fb0801 <fbhatti0801@gmail.com>
2023-08-17 18:18:55 +0100

    short path get started 2

--- short_path_finder.py
+++ short_path_finder.py
@@ -15,6 +15,14 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i,j*2,value, BLUE)
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
@@ -23,7 +31,7 @@ def main(stdscr):
 
 
     stdscr.clear()
-    stdscr.addstr(0,0)
+    print(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
