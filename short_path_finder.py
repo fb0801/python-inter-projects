ceaa0a120935bcc0a70138d050f759e7207a2125
fb0801 <fbhatti0801@gmail.com>
2023-08-17 18:01:55 +0100

    short path get started

--- short_path_finder.py
+++ short_path_finder.py
@@ -15,5 +15,17 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
-def main():
-    
+def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+
+    blue_and_black = curses.color_pair(1)
+
+
+    stdscr.clear()
+    stdscr.addstr(0,0)
+    stdscr.refresh()
+    stdscr.getch()
+
+
+wrapper(main)
