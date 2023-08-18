c53af50a109e7afd143150cc651f0146e790eef6
fb0801 <fbhatti0801@gmail.com>
2023-08-18 16:01:53 +0100

    short path 3

--- short_path_finder.py
+++ short_path_finder.py
@@ -23,6 +23,23 @@ def print_maze(maze, stdscr, path=[]):
         for j, value in enumerate(row):
             stdscr.addstr(i,j*2,value, BLUE)
 
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i,j
+            
+    return None
+
+
+def find_path():
+    start = 'O'
+    end = 'X'
+    start_pos = find_start(maze, start)
+
+    q = queue.Queue()
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
