237e152acf4b14540d54ff4d663856e63339bfa5
fb0801 <fbhatti0801@gmail.com>
2023-10-16 19:18:54 +0100

    targets collide

--- aim-trainer/main.py
+++ aim-trainer/main.py
@@ -44,6 +44,10 @@ class Target:
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.8)
         pygame.draw.circle(win, self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win, self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
+    
+    def collide(self, x, y):
+        dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
+        return dis <= self.size
 
 def draw(win, targets):
     win.fill(BG_COLOR)
@@ -59,10 +63,17 @@ def main():
     targets = []
     clock = pygame.time.Clock()
 
+    target_pressed = 0
+    clicks = 0
+    start_time = time.time()
+    misses = 0
+
     pygame.time.set_timer(TARGET_EVENT,TARGET_INCREMENT)
 
     while run:
         clock.tick(60)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -75,12 +86,21 @@ def main():
                 target = Target(x,y)
                 targets.append(target)
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True  
+                clicks +=1 
+
         for target in targets:
             target.update()
             
             #remove targets from screen
             if target.size <= 0:
                 targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):
+                targets.remove(target)
+                target_pressed += 1
 
         draw(WIN, targets)
 
